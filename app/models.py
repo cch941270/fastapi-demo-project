8d02450967f853fb7d95bd4efe5a4f7779cdbc8d
cch94 <cch1270@tutamail.com>
2025-12-19 19:19:53 +0800

    Add unique constraint to user username

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime, UTC
+from sqlalchemy import String
 from sqlmodel import Column, DateTime, Field, SQLModel
 import uuid
 
@@ -7,7 +8,7 @@ class User(SQLModel, table=True):
     __tablename__: str = "users"
 
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    username: str
+    username: str = Field(sa_column=Column(String(50), unique=True))
     hashed_password: str
     created_at: datetime = Field(
         sa_column=Column(DateTime(timezone=True)),
