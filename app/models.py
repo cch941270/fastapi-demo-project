8ed16519c4ad98109525cf9f46f4887a884740e0
cch94 <cch1270@tutamail.com>
2025-12-20 16:27:25 +0800

    Implement user authentication and login

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(SQLModel, table=True):
     __tablename__: str = "users"
 
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    username: str = Field(sa_column=Column(String(50), unique=True))
+    username: str = Field(sa_column=Column(String(50), unique=True, index=True))
     hashed_password: str
     created_at: datetime = Field(
         sa_column=Column(DateTime(timezone=True)),
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from app.auth import (
+    ACCESS_TOKEN_EXPIRE_DAYS,
+    create_access_token,
+    get_current_user,
+    hash_password,
+    Token,
+    verify_password,
+)
 from app.db import (
     begin_engine_and_create_tables,
     get_async_session,
@@ -5,11 +13,12 @@ from app.db import (
 )
 from app.models import User
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Depends
-from passlib.hash import pbkdf2_sha256
+from datetime import datetime, timedelta, UTC
+from fastapi import Depends, FastAPI, Form, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import select
-
+from typing import Annotated
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,34 +26,54 @@ async def lifespan(app: FastAPI):
     yield
     await dispose_async_engine()
 
-
 app = FastAPI(lifespan=lifespan)
 
 
-def hash_password(password) -> str:
-    return pbkdf2_sha256.hash(password)
+@app.get("/users/me", response_model=User)
+async def read_user_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
 
 
-def verify_password(password, hash) -> bool:
-    return pbkdf2_sha256.verify(password, hash)
+@app.post("/token/")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: AsyncSession = Depends(get_async_session),
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    statement = select(User).where(User.username == form_data.username)
+    results = await session.execute(statement)
+    user = results.scalar()
+    if user is None:
+        raise credentials_exception
+
+    is_password_verified = verify_password(form_data.password, user.hashed_password)
+    if not is_password_verified:
+        raise credentials_exception
+
+    access_token_expires = timedelta(days=ACCESS_TOKEN_EXPIRE_DAYS)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
 
-@app.get("/users")
-async def list_users(session: AsyncSession = Depends(get_async_session)):
-    result = await session.execute(select(User))
-    users = [row[0] for row in result.all()]
-    return users
-
-
-@app.get("/create_users")
-async def create_users(session: AsyncSession = Depends(get_async_session)):
-    user1 = User(username="user1", hashed_password=hash_password("user1pw"))
-    user2 = User(username="user2", hashed_password=hash_password("user2pw"))
-    user3 = User(username="user3", hashed_password=hash_password("user3pw"))
-
-    session.add(user1)
-    session.add(user2)
-    session.add(user3)
-
-    await session.commit()
-    return [user1, user2, user3]
+@app.post("/user/create/")
+async def create_user(
+    username: Annotated[str, Form()],
+    password: Annotated[str, Form()],
+    confirm_password: Annotated[str, Form()],
+    session: AsyncSession = Depends(get_async_session),
+):
+    if password != confirm_password:
+        raise HTTPException(status_code=422, detail="Two passwords are not the same.")
+    try:
+        new_user = User(username=username, hashed_password=hash_password(password))
+        session.add(new_user)
+        await session.commit()
+        return {"message": "New user created"}
+    except Exception as e:
+        raise HTTPException(status_code=422, detail=str(e))
