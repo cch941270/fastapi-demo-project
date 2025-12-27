1e19f629daf11c4ac76e43cb7535837ef6d531e5
cch94 <cch1270@tutamail.com>
2025-12-27 13:40:40 +0800

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ from .routers import discussion_threads
 from contextlib import asynccontextmanager
 from datetime import timedelta
 from fastapi import Depends, FastAPI, Form, HTTPException, Request, status
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import select
@@ -31,6 +32,15 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(discussion_threads.router)
+
+origins = ["http://localhost:5173"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
